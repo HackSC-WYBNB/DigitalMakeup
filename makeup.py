ce4a5e8e9acb9414ed26f581e6d5b9e10a800056
Windy Feng <1745500559@qq.com>
2021-02-20 14:15:28 +0800

    Initial commit

---
+++ makeup.py
@@ -0,0 +1,5 @@
+def putMakeupOn(faceImage, r, g, b, a):
+    """
+    This function reads from a face image object(already loaded), and puts color RGBA on the lips, returns the altered image
+    """
+    return faceImage
